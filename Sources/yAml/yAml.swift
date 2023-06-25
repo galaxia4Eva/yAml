74bbabbbf0488c9e59fda14ed541cd4559e652a8
η Δεμερζελ <578729+galaxia4Eva@users.noreply.github.com>
2023-06-25 14:30:34 +0200

    added draft of basic parsers

--- Sources/yAml/yAml.swift
+++ Sources/yAml/yAml.swift
@@ -1,2 +1,25 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Parsing
+
+let Block = Parse { "- " }
+
+let Mapping = Parse { ": " }
+
+let Comment = Parse { "#" }
+
+let Separator = Parse { "---" }
+
+let EndOfDocument = Parse { "..." }
+
+let Anchor = Parse { "&" }
+
+let Alias = Parse { "*" }
+
+let ComplexMapping = Parse { "? " }
+
+let LiteralStyle = Parse { "|" }
+
+let FoldedStyle = Parse { ">" }
+
+let Tag = { "!" }
