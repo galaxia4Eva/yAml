1aa64f477e999b5ca750be187ef788863b835691
η Δεμερζελ <578729+galaxia4Eva@users.noreply.github.com>
2023-06-25 07:57:37 +0200

    initial commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "yAml",
+    platforms:[
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(name: "yAml", targets: ["yAml"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.12.1")
+    ],
+    targets: [
+        .target(name: "yAml",dependencies: [.product(name: "Parsing", package: "swift-parsing")]),
+        .testTarget(name: "yAmlTests", dependencies: ["yAml"]),
+    ]
+)
