7b780dd359c233beee7830ee74078979001db4d7
η Δεμερζελ <578729+galaxia4Eva@users.noreply.github.com>
2023-06-30 10:41:31 +0200

    made macOS platform to be v10_15

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "yAml",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v10_15)
     ],
     products: [
         .library(name: "yAml", targets: ["yAml"])
