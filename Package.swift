b46fd87df4ba0190dd58f395fa9d0cd5ea74ab2e
η Δεμερζελ <578729+galaxia4Eva@users.noreply.github.com>
2023-06-30 10:39:32 +0200

    formatted Package.swift

--- Package.swift
+++ Package.swift
@@ -4,23 +4,19 @@
 import PackageDescription
 
 let package = Package(
-        name: "yAml",
-        platforms:[
+    name: "yAml",
+    platforms: [
         .macOS(.v13)
-        ],
-        products: [
+    ],
+    products: [
         .library(name: "yAml", targets: ["yAml"])
-        ],
-        dependencies: [
+    ],
+    dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.12.1"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "12.0.1"),
-        ],
-        targets: [
-        .target(
-            name: "yAml",
-            dependencies: [.product(name: "Parsing", package: "swift-parsing")]),
-        .testTarget(
-            name: "LibTests",
-            dependencies: ["yAml", "Nimble"])
-        ]
+        .package(url: "https://github.com/Quick/Nimble.git", from: "12.0.1")
+    ],
+    targets: [
+        .target(name: "yAml", dependencies: [.product(name: "Parsing", package: "swift-parsing")]),
+        .testTarget(name: "LibTests", dependencies: ["yAml", "Nimble"])
+    ]
 )
